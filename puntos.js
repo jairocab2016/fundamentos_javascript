bce3d2f3eba0a87060c98493f3a7b31df604a56f
jairo <jairo.cab17@gmail.com>
2018-04-03 01:49:09 -0300

    se agrega metodos a clases

--- puntos.js
+++ puntos.js
@@ -1,20 +1,52 @@
-const p1 = {
-  x: 0,
-  y: 4
+// Creando clases con prototype
+// function Punto(x, y) {
+//   this.x = x
+//   this.y = y
+// }
+// Punto.prototype.moverEnX = function moverEnX(x) {
+//   this.x += x
+// }
+// Punto.prototype.moverEnY = function moverEnY(y) {
+//   this.y += y
+// }
+// Punto.prototype.distancia = function distancia(p) {
+//   const x = this.x - p.x
+//   const y = this.y - p.y
+//   return Math.sqrt(x * x + y * y)
+// }
+
+//Creando clases con Object.create
+const Punto = {
+  init: function init(x, y) {
+    this.x = x
+    this.y = y
+  },
+  moverEnX: function moverEnX(x) {
+    this.x += x
+  },
+  moverEnY: function moverEnY(y) {
+    this.y += y
+  },
+  distancia: function distancia(p) {
+    const x = this.x - p.x
+    const y = this.y - p.y
+
+    return Math.sqrt(x * x + y * y)
+  }
+
+
 }
 
-const p2 = {
-  x: 3,
-  y: 0
-}
+const p1 = Object.create(Punto)
+p1.init(0, 4)
+const p2 = Object.create(Punto)
+p2.init(3, 0)
 
-function distancia(p1, p2) {
-  const x = p1.x - p2.x
-  const y = p1.y - p2.y
-
-  return Math.sqrt(x * x + y * y);
-}
-
-console.log(distancia(p1, p2));
-console.log(distancia(p1, { x: 20, y: -7 }))
-console.log(distancia(p1, { horizontal: 20, vertical: -7 })) // NaN
+// const p1 = new Punto(0, 4)
+// const p2 = new Punto(3, 0)
+console.log(p1.distancia(p2))
+console.log(p2.distancia(p1))
+p1.moverEnX(10)
+console.log(p1.distancia(p2))
+p2.moverEnY(-4)
+console.log(p1.distancia(p2))
