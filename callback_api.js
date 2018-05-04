1856e462ba56b48d66665a9eca4d38ef2a0e1cd0
jairo <jairo.cab17@gmail.com>
2018-05-04 18:06:57 -0300

    se modifica callback

--- callback_api.js
+++ callback_api.js
@@ -15,54 +15,54 @@ const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest //usar en caso d
 
 function get(URL) {
   return new Promise((resolve, reject) => {
-      const xhr = new XMLHttpRequest();
+    const xhr = new XMLHttpRequest();
 
-      xhr.onreadystatechange = function() {
-        const DONE = 4
-        const OK = 200
-        if (this.readyState === DONE) {
-          if (this.status === OK) {
-            //Todo OK
-            resolve(JSON.parse(this.responseText))
-          } else {
-            //Jubo un error
-            reject(newError(`Se produjo un error al realizar el request ${this.status}`))
-          }
+    xhr.onreadystatechange = function() {
+      const DONE = 4;
+      const OK = 200
+      if (this.readyState === DONE) {
+        if (this.status === OK) {
+          //Todo OK
+          resolve(JSON.parse(this.responseText))
+        } else {
+          //Jubo un error
+          reject(newError(`Se produjo un error al realizar el request ${this.status}`))
         }
       }
-      xhr.open('GET', URL);
-      xhr.send(null);
-    })
-  }
+    }
+    xhr.open('GET', URL);
+    xhr.send(null);
+  })
+}
 
-  /* Funcion para tratar el error */
-  function handleError(err) {
-    console.log(`Request failed: ${err}`)
-  }
+/* Funcion para tratar el error */
+function handleError(err) {
+  console.log(`Request failed: ${err}`)
+}
 
 let luke
-  /*llamada al metodo */
-  get('https://www.swapi.co/api/people/1/')
-		.then((response) => {
-			luke = response
-			return get(luke.homeworld)
-		})
-		.then((homeworld) => {
-			luke.homeworld = homeworld
-      console.log(`${luke.name} nació en ${luke.homeworld.name}`)
-		})
-		.catch(err => handleError(err))
+/*llamada al metodo */
+get('https://www.swapi.co/api/people/1/')
+  .then((response) => {
+    luke = response
+    return get(luke.homeworld)
+  })
+  .then((homeworld) => {
+    luke.homeworld = homeworld
+    console.log(`${luke.name} nació en ${luke.homeworld.name}`)
+  })
+  .catch(err => handleError(err))
 
-    // if (err) return handleError(err)
-    //
-    // /* Para traer el mundo */
-    // get(luke.homeworld, function onHomeworldResponse(err, homeworld) {
-    //   if (err) return handleError(err)
-    //
-    //   luke.homeworld = homeworld
-    //   console.log(`${luke.name} nació en ${luke.homeworld.name}`)
-    // })
-    //
-    // console.log(`Request succeded`)
-    // console.log('luke', luke)
-  // })
+// if (err) return handleError(err)
+//
+// /* Para traer el mundo */
+// get(luke.homeworld, function onHomeworldResponse(err, homeworld) {
+//   if (err) return handleError(err)
+//
+//   luke.homeworld = homeworld
+//   console.log(`${luke.name} nació en ${luke.homeworld.name}`)
+// })
+//
+// console.log(`Request succeded`)
+// console.log('luke', luke)
+// })
